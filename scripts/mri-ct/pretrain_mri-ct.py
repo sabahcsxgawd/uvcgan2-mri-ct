ff12be7675a016cb2fd5ff5a4419423211ea5cd8
sabah <sabah72224@gmail.com>
2024-12-12 04:59:13 +0600

    gg

--- scripts/mri-ct/pretrain_mri-ct.py
+++ scripts/mri-ct/pretrain_mri-ct.py
@@ -11,7 +11,7 @@ from uvcgan2.utils.parsers import add_preset_name_parser, add_batch_size_parser,
 def parse_cmdargs():
     parser = argparse.ArgumentParser(description = 'Pretrain MRI-CT generators')
     add_preset_name_parser(parser, 'gen', GEN_PRESETS, 'uvcgan2')
-    add_batch_size_parser(parser, default = 8)
+    add_batch_size_parser(parser, default = 32)
     add_epoch_parser(parser, default = 50)
     return parser.parse_args()
 
